889086aea95ada7afbb3b3c82e361b900721e225
Raman Kooner <ramankooner9@gmail.com>
2018-11-03 02:06:00 -0700

    Update config to use JSON file

--- server/config/config.js
+++ server/config/config.js
@@ -1,9 +1,28 @@
 var env = process.env.NODE_ENV || 'development';
 
-if (env === 'development') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-} else if (env === 'test') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+if (env === 'development' || env === 'test') {
+  var config = require('./config.json');
+  var envConfig = config[env];
+
+  Object.keys(envConfig).forEach((key) => {
+    process.env[key] = envConfig[key];
+  });
 }
+
+// if (env === 'development') {
+//   process.env.PORT = 3000;
+//   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+// } else if (env === 'test') {
+//   process.env.PORT = 3000;
+//   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+// }
+
+
+
+// heroku config
+// heroku config: set NAME=Raman
+// heroku config
+// heroku config:get NAME    - gets the environment variable name
+// heroku config:unset       - unsets the environment variable
+// heroku config:set JWT_SECRET=adsflkfjlfekwfj
+// heroku config 
