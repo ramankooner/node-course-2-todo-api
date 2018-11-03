95e552bacd26803d451b6ea3d95f4aa73ef0e2a1
Raman Kooner <ramankooner9@gmail.com>
2018-11-02 18:35:35 -0700

    Add DELETE /users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -58,6 +58,17 @@ UserSchema.methods.generateAuthToken = function () {
   });
 };
 
+UserSchema.methods.removeToken = function (token) {
+  // $pull lets us remove items from an array that matches a certain criteria
+  var user = this;
+
+  return user.update({
+    $pull: {
+      tokens: {token}
+    }
+  })
+}
+
 // everything we add on to it turns into a model method
 UserSchema.statics.findByToken = function (token) {
   // model methods get called with the model
